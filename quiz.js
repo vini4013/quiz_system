af282d31ae8aab484a99e7a0af5362c4d2e9d94b
vini4013 <kumarivinitayadav@gmail.com>
2020-03-11 17:40:03 +0530

    updated view answer

--- quiz.js
+++ quiz.js
@@ -1,13 +1,46 @@
-console.log(data);
+data=data1;
 
-var start=Math.floor((Math.random() * 20) + 1);
-var interval=Math.floor((Math.random() * 4) + 1);
+
+var start=Math.floor((Math.random() * 10) + 1);
+var interval=Math.floor((Math.random() * 2) + 1);
 var curQue=-1;
 var queArr=[];
 var atempt=0, correct=0;
 var flag=0;
+var mode=0;
 //var que=[];
+var option_a=document.getElementById("option_a");
+var option_b=document.getElementById("option_b");
+var option_c=document.getElementById("option_c");
+var option_d=document.getElementById("option_d");
 
+
+var options=document.getElementsByClassName('option');
+
+function select_que(category){
+	if (category==1){
+		data=data1;
+	}else if (category==2){
+		data=data2;
+	}else if (category==3){
+		data=data3;
+	}else{
+		alert("Cannot load questions")
+	}
+
+  	var count=0;
+  	for(i=start; count<5; i=i+interval){
+	   if (i>9){
+	   	i=i%10;
+	   }
+	   //que.num=i;
+	   queArr[count]= new question(i);
+	   count++;	
+	}
+
+	//console.log(data);
+}
+//console.log(queArr);
 
 var limit = 29;//in seconds
 var time=limit;
@@ -23,54 +56,66 @@ function myTimer() {
 		document.getElementById("timer").innerHTML = "Time left: "+time-- + " s";
 	}
 	else{
-		
 		show_result("timeout");
 	}
   
-  console.log(time);
+  //console.log(time);
 }
 
 function question(queId,response){
 	this.num=queId;
 	this.response=response;
 }
-var count=0;
-for(i=start; count<5; i=i+interval){
-   if (i>19){
-   	i=i%20;
-   }
-   //que.num=i;
-   queArr[count]= new question(i);
-   count++;	
-}
-console.log(queArr);
+
 
 function showQuiz(){
- document.getElementById('userInfoBox').style.display="none";
- document.getElementById('heading1').style.display="none";
- document.getElementById('heading2').style.display="flex";
- document.getElementById('queBox').style.display="flex";
- nextQue(queArr[0].num);
- starTimer();
+ 	document.getElementById('userInfoBox').style.display="none";
+ 	document.getElementById('heading1').style.display="none";
+ 	document.getElementById('heading2').style.display="flex";
+ 	document.getElementById('queBox').style.display="flex";
+ 	document.getElementById('result').style.display="none";
+ 	nextQue(queArr[0].num);
+ 
 }
 
 function loadQuestion(i){
 	//document.getElementById("queNo");
 	document.getElementById("question").innerHTML=data[i].question;
-	document.getElementById("option_a").innerHTML=data[i].a;
-	document.getElementById("option_b").innerHTML=data[i].b;
-	document.getElementById("option_c").innerHTML=data[i].c;
-	document.getElementById("option_d").innerHTML=data[i].d;
+	option_a.innerHTML=data[i].a;
+	option_b.innerHTML=data[i].b;
+	option_c.innerHTML=data[i].c;
+	option_d.innerHTML=data[i].d;
 	document.getElementById("queNo").innerHTML="Question  "+ (curQue+1)+" of 5";
-	showResponse();
+	if (mode=='') {
+		showResponse();
+	}else{
+		showAns();
+	}
+	
+	
 }
 
 function test(){
 	alert();
 }
 
-function setResponse(response){
-	queArr[curQue].response=response;
+function setResponseA(){
+	queArr[curQue].response="a";
+	nextQue();
+	if (flag==1) {show_result();}
+}
+function setResponseB(){
+	queArr[curQue].response="b";
+	nextQue();
+	if (flag==1) {show_result();}
+}
+function setResponseC(){
+	queArr[curQue].response="c";
+	nextQue();
+	if (flag==1) {show_result();}
+}
+function setResponseD(){
+	queArr[curQue].response="d";
 	nextQue();
 	if (flag==1) {show_result();}
 }
@@ -97,20 +142,17 @@ function manage_next(){
 }
 
 function showResponse(){
-	options=document.getElementsByClassName('option');
-	for (var i =0;i<options.length;i++) {
-			options[i].classList.remove("selected");
-	}
+	
+	clear_options();
 	if(queArr[curQue].response){
  		document.getElementById('option_'+queArr[curQue].response).classList.add("selected");	
  	}
- 
 }
 
 function nextQue(){
  	if(curQue<4){
  		curQue++;
- 		console.log(curQue);
+ 		//console.log(curQue);
 	 	loadQuestion(queArr[curQue].num);
 	 	manage_prev();
 	 	manage_next();
@@ -125,13 +167,15 @@ function nextQue(){
 function prevQue(){
   	if (curQue>0) {
   		curQue--;
-  		console.log(curQue);
+  		//console.log(curQue);
 	 	loadQuestion(queArr[curQue].num);
 	 	manage_prev();
 	 	manage_next();
   	}
 }
 function calResult(){
+	atempt=0;
+	correct=0;
 	for(i=0;i<5;i++){
 	 		//id=queArr[i].num;
 	 	if(queArr[i].response){
@@ -146,12 +190,11 @@ function calResult(){
 	 document.getElementById("time").innerHTML= (limit-time)+"s";
 	 document.getElementById("result").style.display="flex";
 	 document.getElementById("queBox").style.display="none";
-
 }
 
 function show_result(submit_type){
 	if(submit_type=='timeout'){
-		alert("Time up! Click ok to show results.");
+		alert("Time up! Click OK to show results.");
 		clearInterval(timer);
 		document.getElementById("timer").style.display="none";
 		calResult();
@@ -163,6 +206,9 @@ function show_result(submit_type){
 	  		calResult();	
  		}else{
  			showResponse();
+ 			if (flag==1) {
+ 				flag=0;
+ 			}
  		}
 
 	}
@@ -170,17 +216,86 @@ function show_result(submit_type){
  	//console.log(correct);
 }
 
+function showAns(){
+   	clear_options();
+	//for (var i =0;i<options.length;i++) {
+	//		options[i].classList.remove("correct");
+	//}
+	selected=queArr[curQue].response;
+	correct=data[queArr[curQue].num].ans;
+   	document.getElementById('option_'+correct).classList.add("correct");
+   	if(!queArr[curQue].response){
+ 		document.getElementById('ans_res').innerHTML="You did not atempt this question";
+ 	}
+ 	else if (selected==correct){
+ 		document.getElementById('ans_res').innerHTML="Your answer was correct";
+ 	}
+ 	else if (selected!=correct){
+ 		document.getElementById('ans_res').innerHTML="Your answer was wrong";
+ 		document.getElementById('option_'+selected).classList.add("wrong");	
+ 	}
+}
+
+function clear_options(){
+	for (var i =0;i<options.length;i++) {
+			options[i].classList.remove("selected");
+	}
+	for (var i =0;i<options.length;i++) {
+			options[i].classList.remove("correct");
+	}
+	for (var i =0;i<options.length;i++) {
+			options[i].classList.remove("wrong");
+	}
+
+}
+
+function viewAnswer(){
+	//alert();
+	document.getElementById('submit_btn').style.display="none";
+	document.getElementById('back_btn').style.display="block";
+	document.getElementById('reset_btn').style.display="none";
+	option_a.removeEventListener('click', setResponseA);
+	//document.getElementById('option_a').classList.remove('option');
+	option_b.removeEventListener('click', setResponseB);
+	option_c.removeEventListener('click', setResponseC);
+	option_d.removeEventListener('click', setResponseD);
+	options=document.getElementsByClassName('option');
+	for (i=0;i<4;i++) {
+		//options[i].classList.add("option_view");
+		options[i].classList.remove("op-hover");
+		//document.getElementsByClassName('option')[i].classList.remove("option");
+	}
+	mode="view";
+	curQue=-1;
+	showQuiz();
+
+}
+
+function reset_ans(){
+	queArr[curQue].response='';
+	showResponse();
+}
 
 
-document.getElementById('sub_btn').addEventListener('click', showQuiz);
+document.getElementById('sub_btn').addEventListener('click', function(){
+	category=document.getElementById('category').value;
+	//alert(category);
+	select_que(category);
+	starTimer();
+	showQuiz();
+});
 
-document.getElementById('option_a').addEventListener('click', function(){setResponse("a"); });
-document.getElementById('option_b').addEventListener('click', function(){setResponse("b"); });
-document.getElementById('option_c').addEventListener('click', function(){setResponse("c"); });
-document.getElementById('option_d').addEventListener('click', function(){setResponse("d"); });
+option_a.addEventListener('click', setResponseA);
+option_b.addEventListener('click', setResponseB);
+option_c.addEventListener('click', setResponseC);
+option_d.addEventListener('click', setResponseD);
 document.getElementById('next_btn').addEventListener('click',nextQue);
 document.getElementById('prev_btn').addEventListener('click',prevQue);
+document.getElementById('reset_btn').addEventListener('click',reset_ans);
 document.getElementById('submit_btn').addEventListener('click', show_result);
+document.getElementById('back_btn').addEventListener('click', calResult);
+document.getElementById('view_ans').addEventListener('click', viewAnswer);
+
 
 
 
